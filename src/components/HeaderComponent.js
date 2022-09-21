ada46e50e6b78bc8decaba9ff30abb9b76ffc4d8
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-09-21 16:59:54 -0700

    Header and Footer

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return(
             <>
-                <Navbar dark color='primary'>
+                <Navbar dark>
                     <div className="container">
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
@@ -15,7 +15,7 @@ class Header extends Component {
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
                                 <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tackle your culinary senses</p>
+                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tackle your culinary senses!</p>
                             </div>
                         </div>
                     </div>
