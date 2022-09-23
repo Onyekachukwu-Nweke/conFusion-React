3c57d3d5fb8fc4bb5448c2942023baab0785b7a4
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-09-23 03:38:19 -0700

    Single Page Apps 2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,10 +6,12 @@ class Header extends Component {
 
     constructor(props){
         super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
             isNavOpen: false
         };
-        this.toggleNav = this.toggleNav.bind(this);
+       
     }
 
     toggleNav() {
